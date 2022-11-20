0130bd29a908145a5feafdf5e8c698f807bedc70
Daniel Subic <daniel.subic@eborsystems.com>
2022-11-20 15:20:31 +1030

    Express now serves vue statically when built

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,22 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 
 const PUBLIC_DIR = '../frontend/public'
 const MAP_DIR = PUBLIC_DIR + '/map_modules/'
 
+// Append headers
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['http://localhost:5173']);
   next();
 });
 
+// Serve static files
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
 app.get('/', (req, res) => {
-  res.send(list_files(MAP_DIR + 'tiles'))
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 })
 
 app.get('/tiles', (req, res) => {
